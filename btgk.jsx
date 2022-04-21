c2f7b6de6875557a757e1d826b68a068d09895d8
binhphancreator <tienkhach19@gmail.com>
2022-04-21 19:26:16 +0700

    done

--- btgk.jsx
+++ btgk.jsx
@@ -27,6 +27,13 @@ var resourceUI = "window { properties:{ resizeable:false, maximizeButton: false 
       btnSaveProj: Button { text:'Save Project'}, \
       btnCreateComp: Button { text:'Create a new composition'}, \
       btnCreateIntro: Button { text:'Create an intro'}, \
+    }, \
+    shapes: Group { \
+      alignment: ['center', 'top'] \
+      strokesDropdownList: DropDownList { \
+        title: 'Strokes', \
+        size: [200, 50], \
+      } \
       btnCreateLayerShape: Button { text:'Create layer shape'}, \
       btnCreateAnimation: Button { text:'Create shape animation'}, \
     }, \
@@ -57,9 +64,45 @@ function createNewComp(name, width, height, pixelAspect, duration, frameRate, co
   return comp
 }
 
-var window = createUI()
+function addDropdownListItem(dropdown, items) {
+  for (var i = 0; i < items.length; i++) {
+    dropdown.add("item", items[i])
+  }
+  dropdown.selection = 0
+}
+
+var strokeChoices = ["1", "2", "5", "10"]
 var latestComp = null
 var latestShapelayer = null
+
+var window = createUI()
+addDropdownListItem(window.mainPanel.shapes.strokesDropdownList, strokeChoices)
+
+function createCarShape() {
+  var shapeWheelLeft = latestShapelayer.property("ADBE Root Vectors Group").addProperty("ADBE Vector Shape - Ellipse")
+  shapeWheelLeft.property("ADBE Vector Ellipse Size").setValue([50, 50])
+  shapeWheelLeft.property("ADBE Vector Ellipse Position").setValue([-75, 200])
+
+  var shapeWheelRight = latestShapelayer.property("ADBE Root Vectors Group").addProperty("ADBE Vector Shape - Ellipse")
+  shapeWheelRight.property("ADBE Vector Ellipse Size").setValue([50, 50])
+  shapeWheelRight.property("ADBE Vector Ellipse Position").setValue([75, 200])
+
+  var shapeBodyCar = latestShapelayer.property("ADBE Root Vectors Group").addProperty("ADBE Vector Shape - Rect")
+  shapeBodyCar.property("ADBE Vector Rect Size").setValue([330, 100])
+  shapeBodyCar.property("ADBE Vector Rect Position").setValue([0, 140])
+
+  var shapeHeadCar = latestShapelayer.property("ADBE Root Vectors Group").addProperty("ADBE Vector Shape - Rect")
+  shapeHeadCar.property("ADBE Vector Rect Size").setValue([40, 40])
+  shapeHeadCar.property("ADBE Vector Rect Position").setValue([175, 140])
+
+  // Fill and Stroke
+  latestShapelayer.property("ADBE Root Vectors Group").addProperty("ADBE Vector Graphic - Fill").property("ADBE Vector Fill Color").setValue([236/255, 240/255, 241/255])
+  var shapeStroke = latestShapelayer.property("ADBE Root Vectors Group").addProperty("ADBE Vector Graphic - Stroke")
+  shapeStroke.property("ADBE Vector Stroke Color").setValue([84/255, 153/255, 199/255])
+
+  var strokeWidth = parseInt(strokeChoices[window.mainPanel.shapes.strokesDropdownList.selection.index])
+  shapeStroke.property("ADBE Vector Stroke Width").setValue(strokeWidth)
+}
 
 window.mainPanel.buttons.btnCreateProj.onClick = createProject
 window.mainPanel.buttons.btnSaveProj.onClick = saveFileWithDialog
@@ -86,22 +129,19 @@ window.mainPanel.buttons.btnCreateIntro.onClick = function() {
   introTextLayer.property("Source Text").fontSize  = 72
   introTextLayer.property("Source Text").justification = ParagraphJustification.CENTER_JUSTIFY;
 }
-window.mainPanel.buttons.btnCreateLayerShape.onClick = function() {
+window.mainPanel.shapes.btnCreateLayerShape.onClick = function() {
   if (!latestComp)
     alert('Bạn cần tạo composition trước khi thực hiện các bước tiếp theo!', 'Error')
   latestShapelayer = latestComp.layers.addShape()
   latestShapelayer.name = "main layer shape"
 }
 
-window.mainPanel.buttons.btnCreateAnimation.onClick = function() {
+window.mainPanel.shapes.btnCreateAnimation.onClick = function() {
   if (!latestShapelayer)
     alert('Bạn cần tạo layer shape trước khi thực hiện tạo animation!', 'Error')
-  var circle = latestShapelayer.property("ADBE Root Vectors Group").addProperty("ADBE Vector Shape - Ellipse")
-  circle.property("ADBE Vector Ellipse Size").setValue([100, 100])
-  circle.property("ADBE Vector Ellipse Position").setValueAtTime(0, [-600, 100])
-  circle.property("ADBE Vector Ellipse Position").setValueAtTime(20, [600, 100])
-  latestShapelayer.property("ADBE Root Vectors Group").addProperty("ADBE Vector Graphic - Fill").property("ADBE Vector Fill Color").setValue([236/255, 240/255, 241/255])
-  var shapeStroke = latestShapelayer.property("ADBE Root Vectors Group").addProperty("ADBE Vector Graphic - Stroke")
-  shapeStroke.property("ADBE Vector Stroke Color").setValue([1, 1, 1])
-  shapeStroke.property("ADBE Vector Stroke Width").setValue(5)
+  createCarShape()
+  var rectComp = [latestComp.width, latestComp.height]
+  latestShapelayer.property('Position').expression = "loopOut()"
+  latestShapelayer.property('Position').setValueAtTime(0, [-10, rectComp[1] / 2])
+  latestShapelayer.property('Position').setValueAtTime(2, [rectComp[0] + 10, rectComp[1] / 2])
 }
