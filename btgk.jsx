8f841c5cffae6a0607546e78c2d3eb918ba76049
binhphancreator <tienkhach19@gmail.com>
2022-04-21 15:31:17 +0700

    create shape animation

--- btgk.jsx
+++ btgk.jsx
@@ -28,7 +28,7 @@ var resourceUI = "window { properties:{ resizeable:false, maximizeButton: false 
       btnCreateComp: Button { text:'Create a new composition'}, \
       btnCreateIntro: Button { text:'Create an intro'}, \
       btnCreateLayerShape: Button { text:'Create layer shape'}, \
-      btnCreateAnimation: Button { text:'Create animation'}, \
+      btnCreateAnimation: Button { text:'Create shape animation'}, \
     }, \
 	}, \
 }"
@@ -59,6 +59,7 @@ function createNewComp(name, width, height, pixelAspect, duration, frameRate, co
 
 var window = createUI()
 var latestComp = null
+var latestShapelayer = null
 
 window.mainPanel.buttons.btnCreateProj.onClick = createProject
 window.mainPanel.buttons.btnSaveProj.onClick = saveFileWithDialog
@@ -84,4 +85,23 @@ window.mainPanel.buttons.btnCreateIntro.onClick = function() {
   introTextLayer.opacity.setInterpolationTypeAtKey(1, KeyframeInterpolationType.HOLD)
   introTextLayer.property("Source Text").fontSize  = 72
   introTextLayer.property("Source Text").justification = ParagraphJustification.CENTER_JUSTIFY;
+}
+window.mainPanel.buttons.btnCreateLayerShape.onClick = function() {
+  if (!latestComp)
+    alert('Bạn cần tạo composition trước khi thực hiện các bước tiếp theo!', 'Error')
+  latestShapelayer = latestComp.layers.addShape()
+  latestShapelayer.name = "main layer shape"
+}
+
+window.mainPanel.buttons.btnCreateAnimation.onClick = function() {
+  if (!latestShapelayer)
+    alert('Bạn cần tạo layer shape trước khi thực hiện tạo animation!', 'Error')
+  var circle = latestShapelayer.property("ADBE Root Vectors Group").addProperty("ADBE Vector Shape - Ellipse")
+  circle.property("ADBE Vector Ellipse Size").setValue([100, 100])
+  circle.property("ADBE Vector Ellipse Position").setValueAtTime(0, [-600, 100])
+  circle.property("ADBE Vector Ellipse Position").setValueAtTime(20, [600, 100])
+  latestShapelayer.property("ADBE Root Vectors Group").addProperty("ADBE Vector Graphic - Fill").property("ADBE Vector Fill Color").setValue([236/255, 240/255, 241/255])
+  var shapeStroke = latestShapelayer.property("ADBE Root Vectors Group").addProperty("ADBE Vector Graphic - Stroke")
+  shapeStroke.property("ADBE Vector Stroke Color").setValue([1, 1, 1])
+  shapeStroke.property("ADBE Vector Stroke Width").setValue(5)
 }
