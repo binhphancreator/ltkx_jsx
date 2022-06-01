10f66fc39649baf076b88e5f2231c473488405ac
binhphancreator <tienkhach19@gmail.com>
2022-06-01 21:28:34 +0700

    rect intro shape

--- btl.jsx
+++ btl.jsx
@@ -52,9 +52,32 @@ function rendeProject() {
   app.project.renderQueue.queueInAME(true);
 }
 
+function createNewComp(name, color) {
+  var comp = app.project.items.addComp(name, 1280, 720, 16/9, 20, 25)
+  if (!color)
+    color = [0, 0, 0]
+  comp.bgColor = color
+  return comp
+}
+
 var window = createUI()
+var introComp = null
+var widthComp = 1280
+var heightComp = 720
 
 window.buttonPane.buttons.btnCreateProj.onClick = createProject
 window.buttonPane.buttons.btnHelp.onClick = function() {
   alert("Hướng dẫn", "Hướng dẫn sử dụng");
+}
+
+window.buttonPane.buttons.btnCreateIntro.onClick = function() {
+  introComp = createNewComp("intro")
+  var introShapeLayer = introComp.layers.addShape()
+  var rectShape = introShapeLayer.property("ADBE Root Vectors Group").addProperty("ADBE Vector Shape - Rect")
+  rectShape.property("ADBE Vector Rect Size").setValueAtTime(0, [30, 0])
+  rectShape.property("ADBE Vector Rect Size").setValueAtTime(0.75, [30, 125])
+  rectShape.property("ADBE Vector Rect Position").setValueAtTime(0.75, [0, 0])
+  rectShape.property("ADBE Vector Rect Position").setValueAtTime(1.25, [-400, 0])
+  introShapeLayer.property("ADBE Root Vectors Group").addProperty("ADBE Vector Graphic - Fill").property("ADBE Vector Fill Color").setValue([1, 1, 1])
+  introComp.openInViewer()
 }
